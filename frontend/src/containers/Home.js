b404c1359c4b998854385e8c70c0f5965e852a73
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-10-15 21:47:56 +0700

    [UPDATE] Home, show list of videos's thumbnail

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { withSiteData, Link } from 'react-static'
-//
+import { Row, Col, Layout } from 'antd'
+import Thumb from '../components/Thumb'
 import logoImg from '../logo.png'
+
+import videos from './videos.json'
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
-    <ul>
-      <li><Link to="/video/8286">video 8286</Link></li>
-      <li><Link to="/video/8788">video 8788</Link></li>
-    </ul>
+    <h1 style={{ textAlign: 'center' }}>Welcome to Doo-Eee-Mai-Tid-F</h1>
+    <Layout className="content" breakpoint='md'>
+      <Row>
+        {videos.map(video => (
+          <Col span={4} key={video.video_id} gutter="24">
+            <Thumb
+              src={video.video_thumbnail}
+              link={`/video/${video.video_id}`}
+              date={video.video_date}
+              title={video.video_name}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Layout>
   </div>
 ))
