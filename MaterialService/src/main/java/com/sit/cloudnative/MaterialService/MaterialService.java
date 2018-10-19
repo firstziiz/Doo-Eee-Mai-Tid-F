738cb9b7467dcc3f85916db4e55472ba632ac7ff
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-10-19 10:38:13 +0700

    [ADD] getMaterialById and deleteMaterial services.

--- MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialService.java
+++ MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialService.java
@@ -8,7 +8,14 @@ public class MaterialService {
     @Autowired
     private MaterialRepository materialRepository;
 
+    public Material getMaterialById(String materialId){
+        return materialRepository.getOne(materialId);
+    }
+
     public Material addMaterial(Material material) {
         return materialRepository.save(material);
+    }
+    public void deleteMaterial(Material material){
+        materialRepository.delete(material);
     }
 }
