25700046222c25167a11de43ca65e5e811bc6845
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-10-20 12:49:30 +0700

    :tada: Init video history service.

---
+++ VideoHistoryService/src/test/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sit.cloudnative.VideoHistoryService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoHistoryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
