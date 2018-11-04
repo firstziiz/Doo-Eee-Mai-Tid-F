35d17fb865dc2cbd221db815c1d5d24c59b13866
SandzoNight <flukemdc@gmail.com>
2018-11-04 15:17:47 +0700

    [UPDATE] Enable JpaAuditing for NoteService

--- VideoService/src/main/java/com/sit/cloudnative/VideoServiceApplication.java
+++ VideoService/src/main/java/com/sit/cloudnative/VideoServiceApplication.java
@@ -2,8 +2,10 @@ package com.sit.cloudnative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VideoServiceApplication {
 	public static void main(String[] args) {SpringApplication.run(VideoServiceApplication.class, args);}
 }
