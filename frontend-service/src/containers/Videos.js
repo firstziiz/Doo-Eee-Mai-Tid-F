4178d3dab5f6ff7fe16782300b051ef3b09f6df8
Kanisorn S <firstziiz.k@gmail.com>
2018-11-07 19:56:04 +0700

    [ADD] UI of video list

--- frontend-service/src/containers/Videos.js
+++ frontend-service/src/containers/Videos.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import Layout from '../components/Core/Layout'
+import { Link } from 'react-static'
 
 class Videos extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>Videos</h1>
+        <div>
+          <h1>All Videos</h1>
+          <Link className="row">
+            {[...Array(12)].map((video, index) => (
+              <Link className="col-3" key={index}>
+                <Link to="/video/1">
+                  <div className="card mb-4 shadow-sm">
+                    <img src="https://placeimg.com/200/125/any" className="card-img-top" />
+                    <div className="card-body">
+                      <p className="card-text text-muted">INT999: Make Up Class </p>
+                      <div className="d-flex flex-column">
+                        <small className="text-muted">9 mins</small>
+                        <small className="text-muted">Dr. Kunchai Sodhom — INT101</small>
+                        <small className="text-muted">09 Nov 2561</small>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </Link>
+            ))}
+          </Link>
+        </div>
       </Layout>
     )
   }
