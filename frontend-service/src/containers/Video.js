828fba2f1b193ccc3294701ada713392d1ef78a0
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-07 22:37:48 +0700

    [UPDATE] fetch video data from videoService

--- frontend-service/src/containers/Video.js
+++ frontend-service/src/containers/Video.js
@@ -3,15 +3,46 @@ import Layout from '../components/Core/Layout'
 import { notification } from 'antd'
 import ReactQuill from 'react-quill'
 import ReactPlayer from 'react-player'
+import axios from 'axios'
+import moment from 'moment'
 import 'react-quill/dist/quill.snow.css'
 
 class Video extends React.Component {
   state = {
-    note: '... your note.'
+    note: '... your note.',
+    url: '',
+    name: '-',
+    date: '-',
+    duration: ''
   }
 
   handleNoteChange = value => {
     this.setState({ note: value })
+  }
+  get videoId() {
+    return this.props.match.params.videoId
+  }
+
+  componentDidMount() {
+    const videoService = 'http://video-service-bold-grysbok.mybluemix.net/video/'
+    axios({
+      url: `${this.videoId}`,
+      baseURL: videoService,
+      method: 'get'
+    })
+      .then(response => {
+        const video = response.data
+        if (video) {
+          this.setState({
+            url: video.player.hls_url,
+            name: video.video_name,
+            date: moment(video.video_date).format('D MMM YYYY'),
+            period: `${moment(video.video_starttime).format('HH:mm:ss')} - ${moment(video.video_endtime)}`,
+            duration: video.video_duration
+          })
+        }
+        return response.data
+      })
   }
 
   saveNote = () => {
@@ -26,13 +57,13 @@ class Video extends React.Component {
         <div className="embed-responsive" >
           <ReactPlayer
             controls
-            url="https://ngestreaming.sit.kmutt.ac.th/INT305/2018-08-17_13-30_INT305-CB2301_,SD,HD,.mp4.urlset/master.m3u8?ngetoken=_-S3gS9KD7zq5WGxp6sXag&ngetime=1541618437"
+            url={this.state.url}
           />
         </div>
         <div className="row py-3">
           <div className="col-4">
             <div className="detail">
-              <h4 className="mb-0">INT999 Makeup Class</h4>
+              <h4 className="mb-0">{this.state.name}</h4>
               <h6 className="text-muted">99,999 Views</h6>
               <div className="text-muted">Duration: 9 mins</div>
               <div className="text-muted">Profressor: Dr. Kunchai Sodhom — INT101</div>
