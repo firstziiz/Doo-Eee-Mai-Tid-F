11640a2dae5883f99277f27d6cc196041eb8fa6c
Kanisorn S <firstziiz.k@gmail.com>
2018-11-07 19:00:12 +0700

    [ADD] UI of Login page

--- frontend-service/src/containers/Login.js
+++ frontend-service/src/containers/Login.js
@@ -4,9 +4,32 @@ import Layout from '../components/Core/Layout'
 class Login extends React.Component {
   render() {
     return (
-      <Layout>
-        <h1>Login</h1>
-      </Layout>
+      <div>
+        <div className="container text-center pt-5">
+          <div className="row">
+            <div className="col-6 offset-3">
+              <div className="card">
+                <div className="card-body">
+                  <form action="#">
+                    <h3>
+                      SITFLIX <small className="d-block">Next Generation of E-learning</small>
+                    </h3>
+                    <div className="form-group text-left">
+                      <label htmlFor="">Student ID:</label>
+                      <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group text-left">
+                      <label htmlFor="">Password:</label>
+                      <input type="password" className="form-control" />
+                    </div>
+                    <button className="btn btn-primary btn-block">Login</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
