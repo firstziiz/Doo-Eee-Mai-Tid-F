732d71160ce4779267fc033eeaf835d6ef44b0c5
Pureewat <pureewat.k@gmail.com>
2018-11-10 00:01:47 +0700

    [ADD] cors for video service

--- VideoService/src/main/java/com/sit/cloudnative/VideoServiceApplication.java
+++ VideoService/src/main/java/com/sit/cloudnative/VideoServiceApplication.java
@@ -3,9 +3,11 @@ package com.sit.cloudnative;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@CrossOrigin("*")
 public class VideoServiceApplication {
 	public static void main(String[] args) {SpringApplication.run(VideoServiceApplication.class, args);}
 }
