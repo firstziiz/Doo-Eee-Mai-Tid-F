76be0e2bd0ecf35e790f852fccc363e183702cc2
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 10:40:57 +0700

    [ADD] Get,Delete user's history controller.

--- VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryController.java
+++ VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class VideoHistoryController {
     @Autowired
@@ -22,4 +24,27 @@ public class VideoHistoryController {
         videoHistory = videoHistoryService.createNewHistory(videoHistory);
         return new ResponseEntity<VideoHistory>(videoHistory,HttpStatus.CREATED);
     }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/users/{userId}/video-history"
+    )
+    public ResponseEntity<List<VideoHistory>> getHistory(@PathVariable("userId")int userId){
+        List<VideoHistory> videoHistories = videoHistoryService.getUserHistory(userId);
+        return new ResponseEntity<List<VideoHistory>>(videoHistories,HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "/users/{userId}/video-history"
+    )
+    public ResponseEntity deleteHistory(
+            @PathVariable("userId")int userId,
+            @RequestParam("video_id")int videoId)
+    {
+        VideoHistory videoHistory = new VideoHistory(new CompositePrimaryKey(userId,videoId));
+        videoHistoryService.deleteUserHistory(videoHistory);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 }
