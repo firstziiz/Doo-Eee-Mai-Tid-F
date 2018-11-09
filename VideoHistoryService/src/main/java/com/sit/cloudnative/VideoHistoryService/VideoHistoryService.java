5bbad201497261233245c04e9356251850de7acd
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 10:38:34 +0700

    [ADD] Get,Delete user's history.

--- VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryService.java
+++ VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryService.java
@@ -3,6 +3,8 @@ package com.sit.cloudnative.VideoHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class VideoHistoryService {
     @Autowired
@@ -10,5 +12,11 @@ public class VideoHistoryService {
 
     public VideoHistory createNewHistory(VideoHistory videoHistory){
         return videoHistoryRepository.save(videoHistory);
+    }
+    public List<VideoHistory> getUserHistory(int userId){
+        return videoHistoryRepository.findByIdUserId(userId);
+    }
+    public void deleteUserHistory(VideoHistory videoHistory){
+        videoHistoryRepository.delete(videoHistory);
     }
 }
