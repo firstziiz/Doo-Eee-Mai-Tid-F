95ab0b9836a6e8617c051ac467ea4aea4073d917
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 20:42:09 +0700

    [UPDATE] Get attribute value from env.

--- MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MinioStorageService.java
+++ MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MinioStorageService.java
@@ -2,6 +2,14 @@ package com.sit.cloudnative.MaterialService;
 
 import io.minio.MinioClient;
 import io.minio.errors.*;
+import org.omg.CORBA.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -12,11 +20,19 @@ import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+@Component
 public class MinioStorageService {
+
     MinioClient minioClient;
-    String url = "";
-    String bucketName = "";
-    String accessKey = "";
-    String secretKey = "";
+
+    @Value("${storage.minio.url}")
+    private String url;
+    @Value("${storage.minio.bucket-name}")
+    private String bucketName;
+    @Value("${storage.minio.access-key}")
+    private String accessKey;
+    @Value("${storage.minio.secret-key}")
+    private String secretKey;
+
 
 }
