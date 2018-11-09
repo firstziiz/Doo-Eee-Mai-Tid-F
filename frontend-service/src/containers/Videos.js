08873a3ab2514bb4ebd98fae390e42c44384eb39
Kanisorn S <firstziiz.k@gmail.com>
2018-11-09 23:35:07 +0700

    [UPDATE] fixed error when have no videos

--- frontend-service/src/containers/Videos.js
+++ frontend-service/src/containers/Videos.js
@@ -35,28 +35,29 @@ class Videos extends React.Component {
     return (
       <Layout>
         <div>
-          <h1>All Videos</h1>
+          <h1>{this.subjectId ? `Subject ${this.subjectId}` : 'All Videos'}</h1>
           <Link className="row">
-            {this.state.videos.map((video, index) => (
-              <Link className="col-4" key={index}>
-                <Link to={`/video/${video.video_id}`}>
-                  <div className="card mb-4 shadow-sm">
-                    <img src={video.video_thumbnail} className="card-img-top" />
-                    <div className="card-body">
-                      <TitleOverflow className="card-text text-muted">
-                        {video.video_name}
-                      </TitleOverflow>
-                      <div className="d-flex flex-column">
-                        <small className="text-muted">{video.teacher.teacher_name}</small>
-                        <small className="text-muted">
-                          {moment(video.video_date).format('DD MMM YY')}
-                        </small>
+            {this.state.videos &&
+              this.state.videos.map((video, index) => (
+                <Link className="col-4" key={index}>
+                  <Link to={`/video/${video.video_id}`}>
+                    <div className="card mb-4 shadow-sm">
+                      <img src={video.video_thumbnail} className="card-img-top" />
+                      <div className="card-body">
+                        <TitleOverflow className="card-text text-muted">
+                          {video.video_name}
+                        </TitleOverflow>
+                        <div className="d-flex flex-column">
+                          <small className="text-muted">{video.teacher.teacher_name}</small>
+                          <small className="text-muted">
+                            {moment(video.video_date).format('DD MMM YY')}
+                          </small>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </Link>
-              </Link>
-            ))}
+              ))}
           </Link>
         </div>
       </Layout>
