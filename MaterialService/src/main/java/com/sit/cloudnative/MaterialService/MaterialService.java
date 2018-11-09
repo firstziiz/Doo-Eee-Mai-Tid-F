b4f49e6940d8ab6c4259eadbf5d1bee10aff8423
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 22:22:48 +0700

    [ADD] Get all materials service.

--- MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialService.java
+++ MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialService.java
@@ -3,15 +3,17 @@ package com.sit.cloudnative.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MaterialService {
     @Autowired
     private MaterialRepository materialRepository;
 
+    public List<Material> getAllMaterials(){ return materialRepository.findAll();}
     public Material getMaterialById(String materialId){
         return materialRepository.getOne(materialId);
     }
-
     public Material addMaterial(Material material) {
         return materialRepository.save(material);
     }
