77306fca6b5c9c69fe0f89e370702a8bc5aee129
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 22:33:31 +0700

    [ADD] Custom query method findBySubjectId.

--- MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialRepository.java
+++ MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialRepository.java
@@ -3,6 +3,9 @@ package com.sit.cloudnative.MaterialService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MaterialRepository extends JpaRepository<Material,String> {
+    List<Material> findBySubjectId(int subjectId);
 }
