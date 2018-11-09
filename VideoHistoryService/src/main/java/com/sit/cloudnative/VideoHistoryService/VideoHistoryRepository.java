d4573e7f71ca1b4565752e48f18a336c41ac7b63
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-09 10:40:24 +0700

    [ADD] custom method findByUserId (find user_id in compositePK).

--- VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryRepository.java
+++ VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryRepository.java
@@ -3,6 +3,10 @@ package com.sit.cloudnative.VideoHistoryService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideoHistoryRepository extends JpaRepository<VideoHistory,Integer> {
+
+    List<VideoHistory> findByIdUserId(int userId);
 }
