dceb9bb9d7ce68017d9d852a3de506078db40125
Pureewat <pureewat.k@gmail.com>
2018-11-23 23:12:28 +0700

    [ADD] token check for video service

---
+++ VideoService/src/main/java/com/sit/cloudnative/Exception/JWTException.java
@@ -0,0 +1,26 @@
+package com.sit.cloudnative.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class JWTException extends HttpErrorException {
+    public JWTException() {
+    }
+
+    public JWTException(String message) {
+        super(message);
+    }
+
+    public JWTException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public JWTException(Throwable cause) {
+        super(cause);
+    }
+
+    public JWTException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
