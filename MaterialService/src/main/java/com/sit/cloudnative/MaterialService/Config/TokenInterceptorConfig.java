bb0080954225eee2e3b84c691755a1c143eeed91
Pureewat <pureewat.k@gmail.com>
2018-11-24 11:50:18 +0700

    [ADD] token for material service

---
+++ MaterialService/src/main/java/com/sit/cloudnative/MaterialService/Config/TokenInterceptorConfig.java
@@ -0,0 +1,27 @@
+package com.sit.cloudnative.MaterialService.Config;
+
+import com.sit.cloudnative.MaterialService.Interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+@Configuration
+public class TokenInterceptorConfig implements WebMvcConfigurer {
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor());
+    }
+
+    @Bean
+    public MappedInterceptor tokenInterceptor() {
+        return new MappedInterceptor(
+                new String[] {"**", ""},
+                new TokenInterceptor(JWT_SECRET));
+    }
+}
