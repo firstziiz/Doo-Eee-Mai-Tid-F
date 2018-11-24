e06670a9f8d46b37bccccf551caafac37e774046
Patipat_Bang <basza120240@gmail.com>
2018-11-24 16:02:18 +0700

    เเยก config ออกจาก code

--- VideoService/src/main/java/com/sit/cloudnative/VideoService/VideoAdapter.java
+++ VideoService/src/main/java/com/sit/cloudnative/VideoService/VideoAdapter.java
@@ -1,15 +1,19 @@
 package com.sit.cloudnative.VideoService;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class VideoAdapter {
 
+  @Value("${nge.api.path}")
+    private String api_Nge;
+
   public Video getVideoById(Long videoId){
     try {
       RestTemplate restTemplate = new RestTemplate();
-      String url = String.format("https://ngelearning.sit.kmutt.ac.th/api/v0/video/%d", videoId);
+      String url = String.format(api_Nge + "/api/v0/video/%d", videoId);
       Video video = restTemplate.getForObject(url, Video.class);
       return video;
     } catch (Exception ex ) {
