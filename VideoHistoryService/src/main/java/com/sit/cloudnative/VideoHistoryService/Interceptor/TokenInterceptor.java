b3365c85d7163e99f1ea9a3af07e6a762e9d5a2f
Pureewat <pureewat.k@gmail.com>
2018-11-24 12:01:16 +0700

    [ADD] token validation for videohistory sergice

---
+++ VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/Interceptor/TokenInterceptor.java
@@ -0,0 +1,58 @@
+package com.sit.cloudnative.VideoHistoryService.Interceptor;
+
+import com.sit.cloudnative.VideoHistoryService.Exception.BadRequestException;
+import com.sit.cloudnative.VideoHistoryService.Exception.JWTException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TokenInterceptor implements HandlerInterceptor {
+    private String SECRET;
+
+    public TokenInterceptor(String secret) {
+        this.SECRET = secret;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = getToken(request);
+        String userId = this.getUserIdFromToken(token);
+        request.setAttribute("userId", userId);
+        return true;
+    }
+
+    private boolean isValidToken (String token){
+        if (token == null) {
+            return false;
+        } else if (token.startsWith("Bearer") == false || token.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getToken (HttpServletRequest httpServletRequest) throws BadRequestException {
+        String token = httpServletRequest.getHeader("Authorization");
+        if (this.isValidToken(token) == false) {
+            throw new BadRequestException("Invalid authorization provided.");
+        }
+        return token;
+    }
+
+    private String getUserIdFromToken(String token) throws JWTException {
+        String userId;
+        String tokenFormat = token.substring(7);
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(this.SECRET)
+                    .parseClaimsJws(tokenFormat);
+            userId = (String) claims.getBody().get("userId");
+        } catch (Exception jwtException) {
+            throw new JWTException(jwtException.getMessage());
+        }
+        return userId;
+    }
+}
