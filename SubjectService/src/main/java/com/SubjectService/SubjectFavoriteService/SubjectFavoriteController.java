918ac76c498fba4de917147d1f00cd522f15760c
Patipat_Bang <basza120240@gmail.com>
2018-11-30 23:37:13 +0700

    Implemented Logger in SubjectFavoriteController

--- SubjectService/src/main/java/com/SubjectService/SubjectFavoriteService/SubjectFavoriteController.java
+++ SubjectService/src/main/java/com/SubjectService/SubjectFavoriteService/SubjectFavoriteController.java
@@ -1,5 +1,7 @@
 package com.SubjectService.SubjectFavoriteService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,14 @@ public class SubjectFavoriteController {
     @Autowired
     private SubjectFavoriteService subjectFavoriteService;
 
+    Logger logger = LoggerFactory.getLogger(SubjectFavoriteController.class);
+
     @RequestMapping(
             method = RequestMethod.POST,
             value = "/subject_favorites"
     )
     public ResponseEntity<SubjectFavorite> addSubjectFavorite(@PathVariable("subject_id") int subjectId, @RequestAttribute("userId") long userId){
+        logger.info("Add subject" + subjectId + "to favorite" + "by user: " + userId);
         return new ResponseEntity<SubjectFavorite>(subjectFavoriteService.addSubjectFavorite(subjectId, userId), HttpStatus.CREATED);
     }
 
@@ -29,6 +34,7 @@ public class SubjectFavoriteController {
             value = "/subject_favorites"
     )
     public ResponseEntity<List<Subject>> getSubjectFavorite(@RequestAttribute("userId") long userId){
+        logger.info("Getting favorite subject of user: "  + userId);
         return new ResponseEntity<List<Subject>>(subjectFavoriteService.getSubjectFavoriteByUserId(userId), HttpStatus.OK);
     }
 }
