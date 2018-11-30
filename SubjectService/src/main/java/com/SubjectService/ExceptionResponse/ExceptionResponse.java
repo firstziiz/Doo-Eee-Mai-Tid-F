c436e19892c05b3cc71f18e42b724fd54fb387eb
Pureewat <pureewat.k@gmail.com>
2018-11-30 22:57:04 +0700

    [UPDATE] using logger with json output

--- SubjectService/src/main/java/com/SubjectService/ExceptionResponse/ExceptionResponse.java
+++ SubjectService/src/main/java/com/SubjectService/ExceptionResponse/ExceptionResponse.java
@@ -1,5 +1,6 @@
 package com.SubjectService.ExceptionResponse;
 
+import com.SubjectService.Logger.AuditLogger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -10,7 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ExceptionResponse {
-    private static Logger logger = LoggerFactory.getLogger(ExceptionResponse.class);
+    private static AuditLogger logger = new AuditLogger(ExceptionResponse.class.getName());
+
     public static void involveResponseWithException(HttpServletRequest request, HttpServletResponse response, Exception exception, HttpStatus httpStatus) {
         ObjectMapper objectMapper = new ObjectMapper();
         ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(
@@ -24,7 +26,7 @@ public class ExceptionResponse {
             String responseBody = objectMapper.writeValueAsString(exceptionResponseBody);
             response.getWriter().write(responseBody);
         } catch (Exception ex) {
-            logger.error(ex.getMessage());
+            logger.error(request, ex.getMessage());
         }
     }
 }
