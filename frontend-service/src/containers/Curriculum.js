ad3df4f126cd95db437e52c282b17e7a0dfaf463
chanonpingpong <chanon.pingpong@gmail.com>
2018-11-30 22:58:00 +0700

    add select color block in curriculum

--- frontend-service/src/containers/Curriculum.js
+++ frontend-service/src/containers/Curriculum.js
@@ -11,7 +11,8 @@ class Curriculum extends React.Component {
   state = {
     programs: [],
     subjects: [],
-    selectedProgram: ''
+    selectedProgram: '',
+    programName: ''
   }
 
   async componentWillMount() {
@@ -23,7 +24,7 @@ class Curriculum extends React.Component {
     } catch (error) {}
   }
 
-  onSelectProgram = async program_id => {
+  onSelectProgram = async (program_id, program_code) => {
     try {
       const subjects = await SubjectService.getSubjectsByProgramId(program_id).then(
         resp => resp.data
@@ -31,7 +32,8 @@ class Curriculum extends React.Component {
 
       this.setState({
         subjects,
-        selectedProgram: program_id
+        selectedProgram: program_id,
+        programCode: program_code
       })
     } catch (error) {}
   }
@@ -49,24 +51,45 @@ class Curriculum extends React.Component {
                 )
               }
               {this.state.programs.map(program => (
-                <a
-                  key={program.program_id}
-                  onClick={() => this.onSelectProgram(program.program_id)}
-                  className="list-group-item d-flex justify-content-between"
-                >
-                  <div>
-                    <h6 className="my-0">{program.program_code}</h6>
-                    <small className="text-muted">{program.program_name}</small>
-                  </div>
-                  <span className="text-muted">
-                    <Icon type="book" theme="outlined" className="mr-2" />
-                  </span>
-                </a>
+                <div>
+                {
+                  this.state.programCode !== program.program_code ? (
+                    <a
+                    key={program.program_id}
+                    onClick={() => this.onSelectProgram(program.program_id, program.program_code)}
+                    className="list-group-item d-flex justify-content-between"
+                  >
+                    <div>
+                      <h6 className="my-0">{program.program_code}</h6>
+                      <small className="text-muted">{program.program_name}</small>
+                    </div>
+                    <span className="text-muted">
+                      <Icon type="book" theme="outlined" className="mr-2" />
+                    </span>
+                  </a>
+                  ) : (
+                    <a
+                    key={program.program_id}
+                    onClick={() => this.onSelectProgram(program.program_id, program.program_code)}
+                    className="list-group-item d-flex justify-content-between"
+                    style={{ backgroundColor: '#000'}}
+                  >
+                    <div>
+                      <h6 className="my-0" style={{ color: '#fff' }}>{program.program_code}</h6>
+                      <small  style={{ color: '#fff'}}>{program.program_name}</small>
+                    </div>
+                    <span className="text-muted">
+                      <Icon type="book" theme="outlined" className="mr-2" style={{ color: '#fff' }} />
+                    </span>
+                  </a>
+                  )
+                }
+                </div>
               ))}
             </div>
           </div>
           <div className="col-5">
-            <h3>Subject of {this.state.selectedProgram}</h3>
+            <h3>Subject of {this.state.programCode}</h3>
             <div className="list-group mb-3">
               {this.state.subjects.map((sj, index) => (
                 <Link
