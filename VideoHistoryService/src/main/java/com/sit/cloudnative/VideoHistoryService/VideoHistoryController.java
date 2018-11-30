9b8d91e06f8e8a2446ffd3f2b096e42610a53bbd
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-11-30 23:05:38 +0700

    [ADD] Allow CrossOrigin.

--- VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryController.java
+++ VideoHistoryService/src/main/java/com/sit/cloudnative/VideoHistoryService/VideoHistoryController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class VideoHistoryController {
     @Autowired
     VideoHistoryService videoHistoryService;
