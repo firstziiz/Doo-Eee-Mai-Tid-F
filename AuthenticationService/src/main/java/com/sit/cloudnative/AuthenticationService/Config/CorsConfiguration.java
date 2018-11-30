a32afdd4b65c02929c77eb28096962eee8439aeb
Pureewat <pureewat.k@gmail.com>
2018-11-30 22:37:40 +0700

    [ADD] cors for authenservice

---
+++ AuthenticationService/src/main/java/com/sit/cloudnative/AuthenticationService/Config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.sit.cloudnative.AuthenticationService.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
+    }
+}
