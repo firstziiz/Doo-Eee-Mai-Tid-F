2eb944fad682d614cc9735c1d79ce5ad62a5829c
Keerati <keerati.jear@gmail.com>
2018-11-30 19:56:05 +0700

    [ADD] add audit logger class in subject service

--- SubjectService/src/main/java/com/SubjectService/Logger/AuditLogger.java
+++ SubjectService/src/main/java/com/SubjectService/Logger/AuditLogger.java
@@ -1,4 +1,41 @@
 package com.SubjectService.Logger;
 
+import com.savoirtech.logging.slf4j.json.logger.JsonLogger;
+import com.savoirtech.logging.slf4j.json.logger.Logger;
+import com.savoirtech.logging.slf4j.json.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class AuditLogger {
+
+    private Logger logger;
+
+    private final String USER_AGENT = "user-agent";
+    private final String IP = "ip";
+
+    public AuditLogger(String caller) {
+        this.logger = LoggerFactory.getLogger(caller);
+    }
+
+    public void logging(HttpServletRequest request, String message, JsonLogger logger) {
+        String userAgent = request.getHeader(USER_AGENT);
+        String ip = request.getRemoteAddr();
+        logger.message(message).field(USER_AGENT, userAgent).field(IP, ip).log();
+    }
+
+    public void warn(HttpServletRequest request, String message) {
+        this.logging(request, message, this.logger.warn());
+    }
+
+    public void info(HttpServletRequest request, String message) {
+        this.logging(request, message, this.logger.info());
+    }
+
+    public void error(HttpServletRequest request, String message) {
+        this.logging(request, message, this.logger.error());
+    }
+
+    public void debug(HttpServletRequest request, String message) {
+        this.logging(request, message, this.logger.debug());
+    }
 }
