d1b4bddc05b944495381964434170c0ce99cdcb0
Pureewat <pureewat.k@gmail.com>
2018-12-01 17:21:51 +0700

    [ADD] manifest for ibmcloud

--- frontend-service/entrypoint.sh
+++ frontend-service/entrypoint.sh
@@ -5,7 +5,8 @@ echo "window.env = {" >> dist/env.js
 echo "  authenticationServiceURL: \"$AUTHENTICATION_SERVICE_URL\"," >> dist/env.js
 echo "  subjectServiceURL: \"$SUBJECT_SERVICE_URL\"," >> dist/env.js
 echo "  videoServiceURL: \"$VIDEO_SERVICE_URL\"," >> dist/env.js
-echo "  videoHistoryServiceURL: \"$VIDEO_HISTORY_SERVICE_URL\"" >> dist/env.js
+echo "  videoHistoryServiceURL: \"$VIDEO_HISTORY_SERVICE_URL\"," >> dist/env.js
+echo "  materialServiceURL: \"$MATERIAL_SERVICE_URL\"" >> dist/env.js
 echo "}" >> dist/env.js
 
 exec "$@"
