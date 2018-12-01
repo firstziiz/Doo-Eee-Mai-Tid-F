50461ef6af932081b652b94513f83fc666e75af8
Kanisorn S <firstziiz.k@gmail.com>
2018-12-01 14:44:12 +0700

    [UPDATE] Curriculum page for support fav subjects

--- frontend-service/src/containers/Curriculum.js
+++ frontend-service/src/containers/Curriculum.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/Core/Layout'
 import { Link } from 'react-static'
-import { Icon } from 'antd'
+import { Icon, notification } from 'antd'
 import SubjectService from '../services/SubjectService'
 import requireAuth from '../utils/requireAuth'
 import Spinner from '../components/Spinner'
+
+const FavIcon = styled.div`
+  font-size: 1.4rem;
+`
 
 @requireAuth
 class Curriculum extends React.Component {
@@ -12,7 +17,8 @@ class Curriculum extends React.Component {
     programs: [],
     subjects: [],
     selectedProgram: '',
-    programName: ''
+    programName: '',
+    favSubjects: []
   }
 
   async componentWillMount() {
@@ -30,12 +36,35 @@ class Curriculum extends React.Component {
         resp => resp.data
       )
 
+      const favSubjects = await SubjectService.getFavoriteSubjects().then(resp => resp.data)
+
       this.setState({
         subjects,
+        favSubjects,
         selectedProgram: program_id,
         programCode: program_code
       })
+
+      await this.getFavoritesSubject()
     } catch (error) {}
+  }
+
+  addFavoriteSubject = async (e, subjectId) => {
+    e.preventDefault()
+
+    await SubjectService.addFavoriteSubject(subjectId)
+      .then(_ => {
+        notification['success']({
+          message: 'เพิ่มวิชาโปรดสำเร็จ',
+          description: 'คุณได้เพิ่มวิชาโปรดสำเร็จแล้ว'
+        })
+      })
+      .catch(_ => {
+        notification['error']({
+          message: 'เพิ่มวิชาโปรดสำเร็จ',
+          description: 'คุณไม่สามารถเพิ่มวิชาโปรดได้ กรุณาติดต่อผู้ดูแลระบบ'
+        })
+      })
   }
 
   render() {
@@ -45,46 +74,28 @@ class Curriculum extends React.Component {
           <div className="col-5">
             <h3>Curriculum</h3>
             <div className="list-group mb-3">
-              {
-                this.state.programs.length === 0 && (
-                  <Spinner />
-                )
-              }
+              {this.state.programs.length === 0 && <Spinner />}
               {this.state.programs.map(program => (
-                <div>
-                {
-                  this.state.programCode !== program.program_code ? (
-                    <a
-                    key={program.program_id}
-                    onClick={() => this.onSelectProgram(program.program_id, program.program_code)}
-                    className="list-group-item d-flex justify-content-between"
-                  >
-                    <div>
-                      <h6 className="my-0">{program.program_code}</h6>
-                      <small className="text-muted">{program.program_name}</small>
-                    </div>
-                    <span className="text-muted">
-                      <Icon type="book" theme="outlined" className="mr-2" />
-                    </span>
-                  </a>
-                  ) : (
-                    <a
-                    key={program.program_id}
-                    onClick={() => this.onSelectProgram(program.program_id, program.program_code)}
-                    className="list-group-item d-flex justify-content-between"
-                    style={{ backgroundColor: '#000'}}
-                  >
-                    <div>
-                      <h6 className="my-0" style={{ color: '#fff' }}>{program.program_code}</h6>
-                      <small  style={{ color: '#fff'}}>{program.program_name}</small>
-                    </div>
-                    <span className="text-muted">
-                      <Icon type="book" theme="outlined" className="mr-2" style={{ color: '#fff' }} />
-                    </span>
-                  </a>
-                  )
-                }
-                </div>
+                <a
+                  key={program.program_id}
+                  className="list-group-item d-flex justify-content-between"
+                  style={{
+                    background: this.state.programCode === program.program_code ? '#000' : '#fff'
+                  }}
+                  onClick={() => this.onSelectProgram(program.program_id, program.program_code)}
+                >
+                  <div>
+                    <h6
+                      className="my-0"
+                      style={{
+                        color: this.state.programCode === program.program_code ? '#fff' : '#000'
+                      }}
+                    >
+                      {program.program_code}
+                    </h6>
+                    <small className="text-muted">{program.program_name}</small>
+                  </div>
+                </a>
               ))}
             </div>
           </div>
@@ -101,6 +112,20 @@ class Curriculum extends React.Component {
                     <h6 className="my-0">{sj.subject_code}</h6>
                     <small className="text-muted">{sj.subject_name}</small>
                   </div>
+                  <FavIcon
+                    className="text-muted d-flex justify-content-center align-items-center"
+                    onClick={e => this.addFavoriteSubject(e, sj.subject_id)}
+                  >
+                    <Icon
+                      type="book"
+                      theme={
+                        this.state.favSubjects.find(f => f.subject_id + '' === sj.subject_id)
+                          ? 'filled'
+                          : 'outlined'
+                      }
+                      className="mr-2"
+                    />
+                  </FavIcon>
                 </Link>
               ))}
             </div>
